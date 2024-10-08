a5fff124de83375819067f6354d32aeb7fc4b02f
jbreit <jason.breitigan@gmail.com>
2024-10-08 16:36:33 -0400

    feat(src/utilities/types/index.ts): add entry
    Added utility type entry src/utilities/types/index.ts

---
+++ src/utilities/types/index.ts
@@ -0,0 +1,8 @@
+export type { Id } from './Id.js';
+export type { Immutable } from './Immutable.js';
+export type { Mutable } from './Mutable.js';
+export type { Nullable } from './Nullable.js';
+export type { Optional } from './Optional.js';
+export type { Prettify } from './Prettify.js';
+export type { Serializable } from './Serializable.js';
+export type { Values } from './Values.js';
